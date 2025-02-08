defb610f4b0806eee65d6464016dfc1299290931
FOODOHOLIC <adarshkohli183@gmail.com>
2025-02-08 18:18:20 +0530

    Initial commit

--- src/VideoStreamingApplication.java
+++ src/VideoStreamingApplication.java
@@ -1,5 +1,76 @@
-public class VideoStreamingApplication {
-    public static void main(String[] args) {
+import java.awt.*;
 
+public class VideoStreamingApplication {
+    private Database database;
+    public int seekTime(String userId, String videoId){
+        WatchedVideo watchedVideo = database.getWatchedVideo(userId, videoId);
+        return watchedVideo.getSeekTime();
+    }
+}
+
+class VideoService{
+    private FileSystem fileSystem;
+
+    public Frame getFrame(String videoId, int timestamp){
+        Video video = fileSystem.getVideo(videoId);
+        return video.getFrame(timestamp);
+    }
+}
+
+class FileSystem{
+    public Video getVideo(String videoId){
+        return null;
+    }
+}
+
+class Database{
+    public WatchedVideo getWatchedVideo(String userId, String videoId){
+       return null;
+    }
+}
+
+class Video{
+    String id;
+    Frame[] frames;
+    String jsonMetaData;
+
+    public Frame getFrame(int timestamp){
+        for (int i = 0; i < frames.length; i++) {
+            if(frames[i].timestamp <= timestamp && frames[i].timestamp+Frame.frameTime>timestamp){
+                return frames[i];
+            }
+        }
+        throw new IndexOutOfBoundsException();
+    }
+}
+
+class Frame{
+    public static int frameTime = 10;
+    byte[] bytes;
+    int timestamp;
+
+    int startTimestamp;
+    int endTimestamp;
+
+}
+
+class User{
+    String id;
+    String name;
+    String email;
+
+    public String getId(){
+        return id;
+    }
+}
+
+class WatchedVideo{
+    String id;
+    String videoId;
+    String userId;
+    int seekTime;
+
+    public int getSeekTime(){
+        return seekTime;
     }
 }
